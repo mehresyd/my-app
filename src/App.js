046c5d7fdb8fe35e8f550c00eb80c47c31b55dbc
mehresyd <93683641+mehresyd@users.noreply.github.com>
2023-04-09 16:53:19 -0500

    new uploads

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+  let todoListItems = [
+    'Laundry',
+    'Dishes',
+    'Sweep the floor',
+    'Feed my cats',
+    'Clean the shower',
+    'Random task A',
+    'Another task',
+    'Another task'
+  ];
+
+  return (
+    <div>
+      <ul>
+        {
+          todoListItems.map((item, index) => {
+            return (<li key={index}>{item}</li>)
+          })
+        }
+      </ul>
+      <input 
+      onChange={(event) => {
+        console.log(event.target.value)
+      }}
+      />
+    </div>
+  );
+}
+
+export default App;
